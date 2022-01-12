d06b262baf5b1a77a7949c373262dad9f42d67fc
Mohammad Hossein Fallahi <MohammadHosseinFallahi@outlook.com>
2022-01-12 13:53:38 +0330

    Add project files.

---
+++ IdentityCodeYad/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IdentityCodeYad.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityCodeYad.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ IdentityCodeYad/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityCodeYad.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public int Price { get; set; }
+}
