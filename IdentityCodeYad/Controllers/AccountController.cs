6a3bb628daa1d33d36976118cec495f73f499bc2
Mohammad Hossein Fallahi <MohammadHosseinFallahi@outlook.com>
2022-01-15 12:46:13 +0330

    Session 3

---
+++ IdentityCodeYad/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using IdentityCodeYad.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCodeYad.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var result = await _userManager.CreateAsync(new IdentityUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                PhoneNumber = model.Phone
+            }, model.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, err.Description);
+                    return View();
+                }
+            }
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ IdentityCodeYad/ViewModels/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityCodeYad.ViewModels;
+
+public class RegisterVM
+{
+    [Required]
+    [MaxLength(250)]
+    public string UserName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [Phone]
+    public string Phone { get; set; }
+    [DataType(DataType.Password)]
+    [Required]
+    public string Password { get; set; }
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password))]
+    public string RePassword { get; set; }
+}
